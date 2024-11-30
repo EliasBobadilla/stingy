05f03f82042c0aac32ec3ca19eb3d7d98dcc6608
EliasBobadilla <elias.bobadilla@gmail.com>
2024-11-30 15:02:16 -0500

    mierda

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
     ],
   },
   transpilePackages: ["next-auth"],
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 };
 
 export default nextConfig;
