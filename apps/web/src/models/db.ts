587999adaa690cffbb8a66378990b5c007bb2cf8
EliasBobadilla <elias.bobadilla@gmail.com>
2024-11-30 20:51:23 -0500

    pls

--- apps/web/src/models/db.ts
+++ apps/web/src/models/db.ts
@@ -4,10 +4,10 @@ import { assertSome } from "@repo/common/utils/validator";
 const region = "us-east-1";
 
 export function createDynamoDB() {
-  const accessKeyId = process.env.AWS_ACCESS_KEY;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
   const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
   
-  assertSome(accessKeyId,"process.env.AWS_ACCESS_KEY")
+  assertSome(accessKeyId,"process.env.AWS_ACCESS_KEY_ID")
   assertSome(secretAccessKey,"process.env.AWS_SECRET_ACCESS_KEY")
 
   return new DynamoDB({
