103ecea9bc799cf5220fd467e6c6f3db856b573a
EliasBobadillaVanta <elias.bobadilla@vanta.com>
2024-12-26 12:04:02 -0500

    fix deployment

--- packages/common/utils/config.ts
+++ packages/common/utils/config.ts
@@ -1,33 +1,51 @@
 import { assertSome } from "../utils/validator";
 
-const isDev = process.env.ENVIRONMENT === "dev";
-
-const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
-assertSome(awsAccessKeyId, "AWS_ACCESS_KEY_ID");
-
-const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-assertSome(awsSecretAccessKey, "AWS_SECRET_ACCESS_KEY");
-
-const awsRegion = process.env.AWS_REGION;
-assertSome(awsRegion, "AWS_REGION");
-
-const jwtSecret = process.env.AUTH_SECRET;
-assertSome(jwtSecret, "AUTH_SECRET");
-
-// const nextAuthSecret = process.env.NEXTAUTH_SECRET;
-// assertSome(nextAuthSecret, "NEXTAUTH_SECRET");
-
-const sessionSecret = process.env.SESSION_SECRET;
-assertSome(sessionSecret, "SESSION_SECRET");
-
-export const config = {
-  isDev,
-  awsAccessKeyId,
-  awsSecretAccessKey,
-  awsRegion,
-  jwtSecret,
-  //  nextAuthSecret,
-  sessionSecret,
+export type Config = {
+  isDev: boolean;
+  awsAccessKeyId: string;
+  awsSecretAccessKey: string;
+  awsRegion: string;
+  jwtSecret: string;
+  sessionSecret: string;
 };
 
-export type Config = typeof config;
+// avoid using .env.* files to reduce the project's initial setup boilerplate
+function getEnvironmentVariables(isDev: boolean): Config {
+  if (isDev) {
+    return {
+      isDev,
+      awsAccessKeyId: "stingyFakeKeyId",
+      awsSecretAccessKey: "stingyFakeSecretAccessKey",
+      awsRegion: "stingyFakeRegion",
+      jwtSecret: "StingyFakeAuthSecret",
+      sessionSecret: "StingyFakeSessionSecret",
+    };
+  }
+
+  const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  assertSome(awsAccessKeyId, "AWS_ACCESS_KEY_ID");
+
+  const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  assertSome(awsSecretAccessKey, "AWS_SECRET_ACCESS_KEY");
+
+  const awsRegion = process.env.AWS_REGION ?? "us-east-1"; // default AWS region
+
+  const jwtSecret = process.env.AUTH_SECRET;
+  assertSome(jwtSecret, "AUTH_SECRET");
+
+  const sessionSecret = process.env.SESSION_SECRET;
+  assertSome(sessionSecret, "SESSION_SECRET");
+
+  return {
+    isDev,
+    awsAccessKeyId,
+    awsSecretAccessKey,
+    awsRegion,
+    jwtSecret,
+    sessionSecret,
+  };
+}
+
+const isDev = !Boolean(process.env.VERCEL);
+
+export const config = getEnvironmentVariables(isDev);
--- packages/common/utils/validator.ts
+++ packages/common/utils/validator.ts
@@ -3,14 +3,14 @@ import { isNonNullish } from "remeda";
 export function assertSome<T>(
   arg?: T,
   objName?: string,
-  logMetadata?: T | string,
+  metadata?: T | string
 ): asserts arg is T {
   if (isNonNullish(arg)) {
     return;
   }
   throw new Error(
     `Found nothing in [${objName}]: ${JSON.stringify(arg)}. ${
-      logMetadata ? "metadata: " + JSON.stringify(logMetadata) : ""
-    } }`,
+      metadata ? "metadata: " + JSON.stringify(metadata) : ""
+    } }`
   );
 }
