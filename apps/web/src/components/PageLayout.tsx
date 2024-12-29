66d2a7c8458e2859f65ebfa0475bc34b7ae54e54
EliasBobadilla <elias.bobadilla@gmail.com>
2024-12-29 09:58:05 -0500

    more fixes

--- apps/web/src/components/PageLayout.tsx
+++ apps/web/src/components/PageLayout.tsx
@@ -9,21 +9,10 @@ type Props = {
 export default function PageLayout({ children, title }: Props) {
   return (
     <>
-      <div
-        style={{
-          boxSizing: "border-box",
-          fontFamily: "system-ui, sans-serif",
-          lineHeight: 1.5,
-          padding: 24,
-        }}
-      >
-        <div style={{ maxWidth: 510 }}>
-          <h1>{title}</h1>
-          {children}
-          <div style={{ marginTop: 24 }}>
-            <LocaleSwitcher />
-          </div>
-        </div>
+      <h1>{title}</h1>
+      {children}
+      <div style={{ marginTop: 24 }}>
+        <LocaleSwitcher />
       </div>
     </>
   );
