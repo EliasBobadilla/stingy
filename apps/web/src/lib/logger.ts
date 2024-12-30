933c90105cdde311a1ae0add99dba44e1463a1b5
EliasBobadillaVanta <elias.bobadilla@vanta.com>
2024-12-30 17:18:09 -0500

    mela2

--- apps/web/src/lib/logger.ts
+++ apps/web/src/lib/logger.ts
@@ -1,19 +1,21 @@
 import { config } from "@repo/common/utils/config";
 import { createLogger, format, transports } from "winston";
 
-const winston = createLogger({
-  format: format.json(),
-  transports: [
-    new transports.File({
-      filename: "tmp/errors.json",
-      level: "error",
-      handleExceptions: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-      format: format.combine(format.timestamp(), format.json({ space: 2 })),
-    }),
-  ],
-});
+const winston =
+  createLogger();
+  //   {
+  //   format: format.json(),
+  //   transports: [
+  //     new transports.File({
+  //       filename: "tmp/errors.json",
+  //       level: "error",
+  //       handleExceptions: true,
+  //       maxsize: 5242880, // 5MB
+  //       maxFiles: 5,
+  //       format: format.combine(format.timestamp(), format.json({ space: 2 })),
+  //     }),
+  //   ],
+  // }
 
 winston.add(
   new transports.Console({
